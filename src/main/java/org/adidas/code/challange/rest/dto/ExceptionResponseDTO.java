e7e903f31c6c69dd0a19cf1177f3a69de95ab0e2
albertoolivan <albertoolivan@gmail.com>
2019-07-06 02:38:09 -0700

    change ExceptionDTP now is not a runtimeExcetion

--- src/main/java/org/adidas/code/challange/rest/dto/ExceptionResponseDTO.java
+++ src/main/java/org/adidas/code/challange/rest/dto/ExceptionResponseDTO.java
@@ -1,23 +1,60 @@
 package org.adidas.code.challange.rest.dto;
 
-public class ExceptionResponseDTO extends RuntimeException {
+public class ExceptionResponseDTO {
 
-	private static final long serialVersionUID = -1031432443437035289L;
+	// main exception message
+	private String message = "";
+	// Exception class
+	private String exception = "";
+	// exception.stackTrace
+	private String stackTrace = "";
 
 	public ExceptionResponseDTO() {
-		super();
-	}
-	
-	public ExceptionResponseDTO(Exception e) {
-		super(e);
 	}
 
 	public ExceptionResponseDTO(String message) {
-		super(message);
+		this.message = message;
 	}
 
-	public ExceptionResponseDTO(String message, Exception cause) {
-		super(message, cause);
+	public ExceptionResponseDTO(String message, String exception) {
+		this.message = message;
+		this.exception = exception;
+	}
+
+	public ExceptionResponseDTO(Exception e) {
+		this.message = e.getMessage();
+		this.exception = e.getClass().getName();
+		this.stackTrace = RestStringUtil.stack2string(e);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getException() {
+		return exception;
+	}
+
+	public void setException(String exception) {
+		this.exception = exception;
+	}
+
+	public String getStackTrace() {
+		return stackTrace;
+	}
+
+	public void setStackTrace(String stackTrace) {
+		this.stackTrace = stackTrace;
+	}
+
+	@Override
+	public String toString() {
+		return "ExceptionResponseDTO [message=" + message + ", exception=" + exception + ", stackTrace=" + stackTrace
+				+ "]";
 	}
 
 }
